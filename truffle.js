33705231683a5770a77045972086b6711bff558b
Alexander Kirillov <saratovsource@gmail.com>
2018-04-02 20:19:24 +0400

    Add Vexor conf

--- truffle.js
+++ truffle.js
@@ -3,7 +3,7 @@ module.exports = {
   // to customize your Truffle configuration!
   networks: {
     development: {
-      host: "ganache",
+      host: process.env.GANACHE_HOST || "localhost",
       port: 8545,
       network_id: "*" // Match any network id
     }
